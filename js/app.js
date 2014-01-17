c5c69490a271f6f941d31e8e122b29c131e5f673
Alex Tucker <tucker.al@gmail.com>
2014-01-17 11:33:03 -0500

    W00t done

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('MyApp', []);
 
 app.controller('TodoController', function($scope){
     $scope.todos = [
-        {name: 'Example', status:'open'},
-        {name: 'Example Done', status:'closed'}
+        {name: 'Example', isDone:false},
+        {name: 'Example Done', isDone:true}
     ];
 });
